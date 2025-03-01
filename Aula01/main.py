ddc21abcc78251e031ec8747dc0dd77757705fea
Thiago Farias <thiagofarias.ti@gmail.com>
2025-03-01 11:10:57 -0300

    Aula 01

---
+++ Aula01/main.py
@@ -0,0 +1,15 @@
+print("Hello World")
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+print(f"A soma dos números é: {num1 + num2}")
+
+# Desafio 1
+CONSTANTE_BONUS = 1000
+
+nome = input("Digite seu nome: ")
+print(f"Olá, {nome}!")
+
+salario = float(input("Digite seu salário: "))
+bonus = float(input("Digite o valor do bônus: "))
+print(f"O salário total é: {salario + bonus + CONSTANTE_BONUS}")
